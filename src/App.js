8f2a916e32fea9f67b9cffc751304e5896e89612
Jakub Karp <kontakt@kubakarp.pl>
2023-04-05 00:56:21 +0200

    Dodaje NavBar w React 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         
         </p>
         <h1>TESTOWE</h1>
         
